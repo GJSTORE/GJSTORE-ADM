407a16ca1c758b15ffde938f12341e4dff988443
GJSTORE <gjfs544@gmail.com>
2026-08-04 00:17:07 -0300

    N4/N7/N18/N19: imagens Cloudinary otimizadas, tema respeita SO, rate-limit no login cliente, backup automatico da planilha

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gjstore-adm-v5';
+const CACHE = 'gjstore-adm-v6';
 const SHELL = ['./admin.html', './gestao_unificada.html', './config.js', './manifest-admin.json'];
 
 self.addEventListener('install', e => {
