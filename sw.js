13b686e88b88a5ee90e1fdd6cb740f8f1ccf479f
GJSTORE <gjfs544@gmail.com>
2026-08-04 10:54:35 -0300

    Destaque: carrossel entre varios ativos, preview ao vivo no admin, contador de clique, busca de produto com foto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gjstore-adm-v11';
+const CACHE = 'gjstore-adm-v12';
 const SHELL = ['./admin.html', './gestao_unificada.html', './config.js', './manifest-admin.json'];
 
 self.addEventListener('install', e => {
