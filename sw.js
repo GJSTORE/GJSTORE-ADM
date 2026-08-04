5e7f441980f9d20a43b4ed06a1e707a37743d0b6
GJSTORE <gjfs544@gmail.com>
2026-08-04 09:24:56 -0300

    FIX CRITICO: modal margem em lote ficava sempre aberto e nao fechava (display:flex inline vencia .hidden)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gjstore-adm-v9';
+const CACHE = 'gjstore-adm-v10';
 const SHELL = ['./admin.html', './gestao_unificada.html', './config.js', './manifest-admin.json'];
 
 self.addEventListener('install', e => {
